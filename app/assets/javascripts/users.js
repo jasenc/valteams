813dea27a79199057ce98104f34c2be3f4dcbb7d
jasenc <jasen.c8@gmail.com>
2015-07-02 09:56:55 -0700

    reverted usersjs

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,37 +1,33 @@
-function stripeResponseHandler(status, response) {
-  //Get a reference to the form
-  var f = $('#new-user');
-  //Get a token from the response
-  var token = response.id;
-  //Add the token to the form
-  f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-  //Submit the form
-  f.get(0).submit();
-}
-
-function main() {
+$(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  // Watch for a form submission
-  $('#form-submit-btn').click(function(event) {
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
     event.preventDefault();
-    $('input[type=submit]').prop('Disabled', true);
+    $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccnum = $('#card_number').val();
-        cvcNum = $('#card_code').val();
-        expMonth = $('card_month').val();
-        expYear = $('card_year').val();
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
     if (!error) {
-      // Get Stripe token
+      // Get the Stripe token:
       Stripe.createToken({
         number: ccNum,
-        code: cvcNum,
+        cvc: cvcNum,
         exp_month: expMonth,
         exp_year: expYear
-      },
-      stripeResponseHandler);
+      }, stripeResponseHandler);
     }
     return false;
-  });
-}
-
-$(document).ready(main());
+  }); // form submission
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
+    var f = $("#new_user");
+    // Get the token from the response:
+    var token = response.id;
+    // Add the token to the form:
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+    // Submit the form:
+    f.get(0).submit();
+  }
+});
