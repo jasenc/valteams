2c9eeb0e6aacc1a4abc88ef2b4f65691aca1bab6
jasenc <jasen.c8@gmail.com>
2015-10-02 12:58:47 -0700

    feat: styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
   def job_title_icon
-    if @user.profile.job_title == "Developer"
-      "<i class='fa fa-code'></i>".html_safe
-    elsif @user.profile.job_title == "Entrapeneur"
+    if @user.profile.job_title == "Contractor"
+      "<i class='fa fa-user'></i>".html_safe
+    elsif @user.profile.job_title == "Consultant"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
-    elsif @user.profile.job_title == "Investor"
-      "<i class='fa fa-dollar'></i>".html_safe
+    elsif @user.profile.job_title == "Recruiter"
+      "<i class='fa fa-user-plus'></i>".html_safe
     end
   end
 end
